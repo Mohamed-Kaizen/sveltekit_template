7dd1a58973c4dfd35fd0b0f4657f7aef759c1447
Mohamed Nesredin <m.n.kaizen@gmail.com>
2023-02-02 15:15:43 +0300

    Update vite.config.ts

--- {{ cookiecutter.project_name|lower|replace(' ', '-') }}/vite.config.ts
+++ {{ cookiecutter.project_name|lower|replace(' ', '-') }}/vite.config.ts
@@ -2,8 +2,6 @@ import { sveltekit } from "@sveltejs/kit/vite"
 
 {% if cookiecutter.use_unplugin_auto_import == 'y' -%}import AutoImport from "unplugin-auto-import/vite"{%- endif %}
 {% if cookiecutter.use_unplugin_svelte_components == 'y' -%}
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import Components from "unplugin-svelte-components/vite"
 {%- endif %}
 {% if cookiecutter.use_unocss == 'y' -%}
